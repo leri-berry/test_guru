b43d005f12fce9b193a101fdb02057592b84ace8
valeria <valeria.ropot2@gmail.com>
2021-10-13 10:34:31 +0300

    add associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_and_belongs_to_many :users, join_table: 'test_users'
+  has_many :questions
+  has_many :test_users
+  has_many :users, through: :test_users
   
   def self.sort_by_category(category_title)
     joins(:category).where(categories: { title: category_title}).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :tests, join_table: 'test_users'
+  has_many :test_users
+  has_many :tests, through: :test_users
 
   def user_tests(test_level)
     tests.where(level: test_level)
