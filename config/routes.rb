de1e65d77f719a40fc8e6f780487dd7bab1c907e
valeria <valeria.ropot2@gmail.com>
2021-11-23 11:28:12 +0300

    edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-
-  get 'sessions/new'
-  get 'users/new'
-
   get :signup, to: 'users#new'
   get :login, to: 'sessions#new'
   delete :logout, to: 'sessions#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_14_083356) do
+ActiveRecord::Schema.define(version: 2021_11_22_181326) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2021_11_14_083356) do
     t.datetime "updated_at", null: false
     t.string "email"
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
