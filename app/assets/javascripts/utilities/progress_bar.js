07a4c9cc757c5f9136e983084aeb0f32b38431dd
valeria <valeria.ropot2@gmail.com>
2022-01-20 23:48:14 +0300

    task_17

--- app/assets/javascripts/utilities/progress_bar.js
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -6,7 +6,7 @@ document.addEventListener('turbolinks:load', function() {
     var currentNumber = progressBar.dataset.currentNumber
     var count = progressBar.dataset.count
 
-    progressBar.style.width = 100/count * (currentNumber - 1) + "%"
+    progressBar.style.width = 100/count * (currentNumber - 1) + "%";
 
   }
 })
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_174415) do
+ActiveRecord::Schema.define(version: 2022_01_16_184402) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -23,6 +26,14 @@ ActiveRecord::Schema.define(version: 2021_12_13_174415) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
