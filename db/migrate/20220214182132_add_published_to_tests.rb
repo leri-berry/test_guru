a7bb87d7047f061c6efcb64187fc9ba7add865af
valeria <valeria.ropot2@gmail.com>
2022-02-14 23:43:59 +0300

    published to test

--- db/migrate/20220214182132_add_published_to_tests.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedToTests < ActiveRecord::Migration[5.2]
-  def change
-    add_column :tests, :published, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_14_182132) do
+ActiveRecord::Schema.define(version: 2022_02_14_203913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2022_02_14_182132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
-    t.boolean "published", default: false
+    t.boolean "published"
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
     t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
